bd67b6b238b8395976ef7ccae2ff2a38d4cd87ff
andychen <strikecbu@gmail.com>
2022-10-04 14:16:56 +0800

    Init

---
+++ src/main/java/com/bondlic/bondsearch/BondSearchApplication.java
@@ -0,0 +1,13 @@
+package com.bondlic.bondsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bondlic/bondsearch/BondSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bondlic.bondsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
